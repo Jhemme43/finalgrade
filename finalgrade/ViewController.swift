ba6d9b084552350d413824885332b51b5af2964c
Jackson <jhhemme43@gmail.com>
2021-10-13 10:54:48 -0600

    MVP

--- finalgrade/ViewController.swift
+++ finalgrade/ViewController.swift
@@ -7,13 +7,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var wantedGrade: UITextField!
+    
+    @IBOutlet weak var currentGrade: UITextField!
+    
+    @IBOutlet weak var finalWeight: UITextField!
+    
+    
+    @IBOutlet weak var myLabel: UITextField!
+    
+    
+    var name = ""
+    
+    var myInt1 = 0.0
+    
+    var Name = ""
+    
+    var myInt2 = 0.0
+    
+    var nAme = ""
+    
+    var myInt3 = 0.0
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func caculateButton(_ sender: Any)
+    {
+    
+        name = wantedGrade.text ?? ""
+        
+        Name = currentGrade.text ?? ""
+        
+        nAme = finalWeight.text ?? ""
+        
+        myInt1 = Double(name) ?? 0.0
+        
+        myInt2 = Double(Name) ?? 0.0
+        
+        myInt3 = Double(nAme) ?? 0.2
+        
+       // var F = (myInt1-myInt2*(1-myInt3/100))/((myInt3/100))
+                                                
+        var G = (myInt1-(myInt2*(1-myInt3/100)))
+        
+        var H = G*100/myInt3
+        
+        myLabel.text = "\(H)"
+        
+        
+        
+        
+    }
+    
+    
+    
+    
 }
 
